738839ee8e04745886a5a66c05965670af5b9bd7
DrantDumani <darnjames@gmail.com>
2023-04-27 01:16:11 -0400

    Removed function from setDisplay calls

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
+import { act } from "react-dom/test-utils";
 
 function Game({ imgData }) {
   const [gameBegin, setGameBegin] = useState(true);
-  const [gamePlaying, setGamePlaying] = useState(false);
+  const [gamePlaying, setGamePlaying] = useState(true);
+  const [fake, setFake] = useState(false);
 
   const handleStartClick = () => {
     setGameBegin(false);
     setGamePlaying(true);
   };
 
+  const checkVic = (e) => {
+    console.log(e.nativeEvent.offsetX);
+    if (e.nativeEvent.offsetX > 200) {
+      act(() => {
+        setFake(true);
+      });
+    }
+  };
+
   return (
     <main>
       {gameBegin && <button onClick={handleStartClick}>Start</button>}
-      {gamePlaying && <img src={imgData.src} alt="game-screen" />}
+      {gamePlaying && (
+        <img src={imgData.src} alt="game-screen" onClick={checkVic} />
+      )}
+      {fake && <p>Finally</p>}
     </main>
   );
 }
