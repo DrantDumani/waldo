187155739a342700008a973be89ebfd219521348
DrantDumani <darnjames@gmail.com>
2023-05-03 17:13:49 -0400

    Restructured game and PlayScreen components

--- src/PlayScreen.js
+++ src/PlayScreen.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PlayerChoices from "./PlayerChoices";
+import UserTime from "./UserTime";
 
 function PlayScreen({ imgData, choices }) {
   const [showSelections, setShowSelections] = useState(false);
-  const [position, setPosition] = useState({ x: 80, y: 0 });
+  const [time, setTime] = useState(0);
+  const [position, setPosition] = useState(null);
 
   const handleImageClick = (e) => {
     setPosition({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY + 10 });
     setShowSelections(true);
   };
 
+  useEffect(() => {
+    setTime((t) => t + 1);
+  }, []);
+
   return (
     <div>
+      <UserTime time={time} />
       <img
         onClick={handleImageClick}
         src={imgData.src}
