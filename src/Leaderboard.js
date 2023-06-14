d26b9bf6087c8d266b51f05eaf8d2cc3a0bbcce7
DrantDumani <darnjames@gmail.com>
2023-06-14 17:47:36 -0400

    Created and passed tests for Leaderboard component

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Leaderboard() {
-  return <h1>Leaderboard</h1>;
+function Leaderboard({ entries }) {
+  return (
+    <main>
+      <h1>Leaderboard</h1>
+      <div>
+        {entries
+          .sort((a, b) => a.time - b.time)
+          .map((entry) => (
+            <div key={entry.id}>
+              <span data-testid="name-field">{entry.name}</span>
+              <span>{entry.date}</span>
+              <span>{entry.time}</span>
+            </div>
+          ))}
+      </div>
+    </main>
+  );
 }
 
 export default Leaderboard;
---
+++ src/Leaderboard.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Leaderboard from "./Leaderboard";
+
+describe("Leaderboard component", () => {
+  it("renders all entries in ascending order of time", () => {
+    const mockEntries = [
+      { time: 2, name: "Bob", id: "foo" },
+      { time: 5, name: "Billy", id: "bar" },
+      { time: 3, name: "Buck", id: "baz" },
+    ];
+    render(<Leaderboard entries={mockEntries} />);
+    const names = screen.getAllByTestId("name-field");
+    expect(names.length).toBe(mockEntries.length);
+    expect(names[0].textContent).toBe("Bob");
+    expect(names[1].textContent).toBe("Buck");
+    expect(names[2].textContent).toBe("Billy");
+  });
+});
